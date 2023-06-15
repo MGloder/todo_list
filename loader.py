9db8d536982a6da77a26821ef6e4dc47124364d7
xu yan <xu_yan@mckinsey.com>
2023-06-15 20:28:39 +0800

    add a loader

---
+++ loader.py
@@ -0,0 +1,17 @@
+import os
+from dotenv import load_dotenv
+
+from app import app
+
+# Load environment variables from .env file
+load_dotenv()
+
+
+def run():
+    # Get the port number from the environment variable, or use the default value 5000
+    port = int(os.getenv('PORT', 5000))
+    app.run(debug=True, port=port)
+
+
+if __name__ == '__main__':
+    run()
