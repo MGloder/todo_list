ed71d689de2837ca56cc4c41f798bdbe0a87da91
xu yan <xu_yan@mckinsey.com>
2023-06-15 20:23:37 +0800

    refactor

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for
+
 from storage import load_todos, save_todos
 
 app = Flask(__name__)
@@ -7,7 +8,8 @@ todos = load_todos()
 
 @app.route('/todos', methods=['GET'])
 def todo_list():
-    return render_template('todos.html', todos=todos)
+    enumerated_todos = enumerate(todos, start=1)
+    return render_template('todos.html', todos=enumerated_todos)
 
 
 @app.route('/todos/add', methods=['POST'])
@@ -18,11 +20,13 @@ def add_todo():
     return redirect(url_for('todo_list'))
 
 
-@app.route('/todos/delete/<todo_item>', methods=['POST'])
-def delete_todo(todo_item):
-    todos.remove(todo_item)
-    save_todos(todos)
-    return redirect(url_for('todo_list'))
+@app.route('/todos/complete/<int:index>', methods=['POST'])
+def complete_todo(index):
+    if 1 <= index <= len(todos):
+        # Mark the todo item as complete (you can define your own logic here)
+        # For demonstration purposes, let's append a prefix '[Complete]' to the todo item
+        todos[index - 1] = '[Complete] ' + todos[index - 1]
+    return redirect('/todos')
 
 
 if __name__ == '__main__':
